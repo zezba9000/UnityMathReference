5b777b592817adf9aafd90d1abb2d7ddd314346a
Andrew Witte <andrew.witte@vrstudios.com>
2016-08-19 16:33:09 -0700

    Added some cross and matrix demos

---
+++ Assets/Matrix/Matrix3FromCrossedVectors/Matrix3FromCrossedVectorsDemo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Reign;
+
+public class Matrix3FromCrossedVectorsDemo : MonoBehaviour
+{
+	float rot;
+
+	void Update()
+	{
+		var forward = new Vec3(Mathf.Cos(rot), Mathf.Sin(rot), 1).Normalize();
+		var up = new Vec3(Mathf.Sin(rot), 1, 0).Normalize();
+		
+		var mat = Mat3.LookAt(forward, up);
+		Debug.DrawRay(Vector3.zero, mat.x.ToVector3(), Color.red);
+		Debug.DrawRay(Vector3.zero, mat.y.ToVector3(), Color.green);
+		Debug.DrawRay(Vector3.zero, mat.z.ToVector3(), Color.blue);
+
+		rot += Time.deltaTime;
+	}
+}
---
+++ Assets/Vectors/Cross3/Cross3Demo.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Reign;
+
+public class Cross3Demo : MonoBehaviour
+{
+	float rot;
+
+	void Update()
+	{
+		var forward = new Vec3(0, 0, 1).Normalize();
+		var up = new Vec3(Mathf.Sin(rot), 1, 0).Normalize();
+		var right = up.Cross(forward);
+		Debug.DrawRay(Vector3.zero, right.ToVector3(), Color.red);
+		Debug.DrawRay(Vector3.zero, up.ToVector3(), Color.green);
+		Debug.DrawRay(Vector3.zero, forward.ToVector3(), Color.blue);
+
+		rot += Time.deltaTime;
+	}
+}
