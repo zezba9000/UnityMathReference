2f05c7dc43133e6a6ede6ffe025815510512e182
Andrew Witte <andrew.witte@vrstudios.com>
2016-08-30 11:12:11 -0700

    Added some transform tests

---
+++ Assets/Vectors/Transform3/Transform3.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Reign;
+
+public class Transform3 : MonoBehaviour
+{
+	void Update()
+	{
+		var vec = new Vec3(0, 0, 1).Normalize();
+		Debug.DrawRay(Vector3.zero, vec.ToVector3(), Color.red);
+
+		var vecTran = vec.Transform(Quat.FromEuler(0, MathUtilities.piHalf * .5f, 0));
+		Debug.DrawRay(Vector3.zero, vecTran.ToVector3(), Color.green);
+	}
+}
