985afbf2677b968f0f35e3b8d218636228bed6b8
Andrew <zezba9000@gmail.com>
2018-05-05 22:24:08 -0700

    removed unused code. tested Unity 2018

--- Assets/Vectors/Transform3/Transform3.cs
+++ Assets/Vectors/Transform3/Transform3.cs
@@ -3,12 +3,16 @@ using UnityMathReference;
 
 public class Transform3 : MonoBehaviour
 {
+	private float rot;
+
 	void Update()
 	{
 		var vec = new Vec3(0, 0, 1).Normalize();
 		Debug.DrawRay(Vector3.zero, vec.ToVector3(), Color.red);
 
-		var vecTran = vec.Transform(Quat.FromEuler(0, MathUtilities.piHalf * .5f, 0));
+		var vecTran = vec.Transform(Quat.FromEuler(0, rot, 0));
 		Debug.DrawRay(Vector3.zero, vecTran.ToVector3(), Color.green);
+
+		rot += Time.deltaTime;
 	}
 }
